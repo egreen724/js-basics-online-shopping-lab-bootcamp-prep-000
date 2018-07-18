a574569e6f24b1bf890ae197539f651783157e64
Erin Green <erin.y.green@gmail.com>
2018-07-18 18:11:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else {
-    cart.slice(0, cart.length)
+    setCart([]);
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
 }
