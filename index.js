6e395e35d48446945dca708c062a936f9beb9dc5
Erin Green <erin.y.green@gmail.com>
2018-07-18 17:21:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,24 +10,24 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- cart.unshift(item);
  item = { 
    itemName: `${item}`, 
    itemPrice: Math.floor(Math.random() * 101) };
+ cart.push(item);
  return `${item.itemName} has been added to your cart.`
 }
 
 function viewCart() {
-  for (let i = 0; i < 1; ) {
+  for (let i = 0; cart.length === 0; ) {
     return "Your shopping cart is empty."
   }
-  for (let i = 0; i == 1; ) {
+  for (let i = 0; cart.length === 1; ) {
     return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`
   }
-  for (let i = 0; i == 2; ){
+  for (let i = 0; cart.length === 2; ){
     return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, and ${getCart()[1].itemName} at $${getCart()[1].itemPrice}.`
   }
-  for (let i = 0; i >= 3; i++){
+  for (let i = 0; cart.length >= 3; i++){
     return `In your cart, you have ${getCart()[i].itemName} at $${getCart()[i].itemPrice}, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}, and ${getCart()[i].itemName} at $${getCart()[i].itemPrice}.`
   }
 }
