ef07609c26a07b137f84ccb37183b4237cc6aa0a
Erin Green <erin.y.green@gmail.com>
2018-07-18 16:56:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,32 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ cart.unshift(item);
+ item = { 
+   itemName: cart[0], 
+   itemPrice: Math.floor(Math.random() * 101) }
+ return `${itemName} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  for (let i = 0; i < 1; ) {
+    return "Your shopping cart is empty."
+  }
+  for (let i = 0; i == 1; ) {
+    return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`
+  }
+  for (let i = 0; i == 2; ){
+    return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, and ${getCart()[1].itemName} at $${getCart()[1].itemPrice}.`
+  }
+  for (let i = 0; i >= 3; i++){
+    return `In your cart, you have ${getCart()[i].itemName} at $${getCart()[i].itemPrice}, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}, and ${getCart()[i].itemName} at $${getCart()[i].itemPrice}.`
+  }
 }
 
 function total() {
-  // write your code here
+  for (i = 0; i < cart.length; i++){
+    return itemPrice[i] + itemPrice.length[i];
+  }
 }
 
 function removeFromCart(item) {
