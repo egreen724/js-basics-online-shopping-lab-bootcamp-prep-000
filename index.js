823990e8f0e80d8d100d98276a9f4cd09802341c
Erin Green <erin.y.green@gmail.com>
2018-07-18 19:08:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,6 +71,6 @@ function placeOrder(cardNumber) {
   }
   else {
     setCart([]);
-    return `Your total cost is $${sumUpPrices()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
   }
 }
